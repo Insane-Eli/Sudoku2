44be05a3ef0b676cbbdd4a07e6b63080ed1ace70
Eli <150296902+Insane-Eli@users.noreply.github.com>
2025-04-23 12:15:42 -0700

    checkDuplicate() complete

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -48,12 +48,12 @@ public class SudokuBoard {
     }
     
     public boolean checkDuplicate() {
-      Set<Integer> set = new HashSet<Integer>();
-      int numbersFound = 0;
       
       //row
       for(int r = 0; r < board.length; r++) {
+         Set<Integer> set = new HashSet<Integer>();
          for(int c = 0; c < board[0].length; c++) {
+         
             int num = board[r][c];
             
             if(num != 0){ // if the num isn't blank, count it as a number
@@ -64,10 +64,63 @@ public class SudokuBoard {
                   set.add(num); // add it to the set
                }
             }
+            
          }
       }
+      
+      //columns
+      for(int c = 0; c < board[0].length; c++) {
+         Set<Integer> set = new HashSet<Integer>();
+         for(int r = 0; r < board.length; r++) {
+         
+            int num = board[r][c];
+            
+            if(num != 0){ // if the num isn't blank, count it as a number
+               if(set.contains(num)){ // if the number is already in the set
+                   System.out.println("duplicate number in column " + c); // theres a duplicate
+                   return false;
+               } else { // if its not
+                  set.add(num); // add it to the set
+               }
+            }
+            
+         }
+      }
+      
+      // blocks
+      for(int s = 1; s <= 9; s++){ // go through spots 1-9
+         Set<Integer> set = new HashSet<Integer>();
+         int[][] minisquare = miniSquare(s);
+         for(int c = 0; c < minisquare[0].length; c++) {   
+            for(int r = 0; r < minisquare.length; r++) {
+               int num = minisquare[r][c];
+               if(num != 0){ // if the num isn't blank, count it as a number
+                  if(set.contains(num)){ // if the number is already in the set
+                      System.out.println("duplicate number in mini square " + s); // theres a duplicate
+                      return false;
+                  } else { // if its not
+                     set.add(num); // add it to the set
+                  }
+               }
+            }
+         }
+      }
+      
       return true;
     }
+    
+    private int[][] miniSquare(int spot) {
+      int[][] mini = new int[3][3];
+      for(int r = 0; r < 3; r++) {
+         for(int c = 0; c < 3; c++) {
+            // whoa - wild! This took me a solid hour to figure out (at least)
+            // This translates between the "spot" in the 9x9 Sudoku board
+            // and a new mini square of 3x3
+            mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
+         }
+      }
+      return mini;
+   }
 
     public String toString() {
       String result = "";
