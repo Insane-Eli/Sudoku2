46168822973c9d778de8ae1d0dcb1130066f2211
Eli <eli@currah.com>
2025-04-26 20:52:55 -0700

    created isSolved() + added test boards and more comments

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -19,7 +19,7 @@ public class SudokuBoard {
                     if (ch == '.') {
                         board[r][c] = 0;
                     } else {
-                       board[r][c] = ch - '0';
+                        board[r][c] = ch - '0';
                     }
                 }
             }
@@ -28,120 +28,153 @@ public class SudokuBoard {
             System.out.println("Cannot find file: " + filename);
         }
     }
-    
 
-    public boolean isValid(){
-        return (validCharacters() && checkDuplicate());
+    public boolean isValid() {
+        return (validCharacters() && noDuplicates()); // if the board is all valid characters with no duplicate numbers, the board is valid
     }
-    
-    private boolean validCharacters(){
-      for(int r = 0; r < board.length; r++) {
-         for(int c = 0; c < board[0].length; c++) {
-            int i = board[r][c];
-            if(!(i >= 0 || i <= 9)){ // if its a number 0-9 or is '.'
-                System.out.println("invalid character");
+
+    private boolean validCharacters() {
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                int i = board[r][c];
+                if (!(i >= 0 || i <= 9)) { // if its a number 0-9 or is '.'
+                    System.out.println("invalid character");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean noDuplicates() {
+
+        // check rows
+        for (int r = 0; r < board.length; r++) { // start with a row
+            Set<Integer> set = new HashSet<Integer>();
+            for (int c = 0; c < board[0].length; c++) { // then check ever column in that row
+
+                int num = board[r][c];
+
+                if (num != 0) { // if the num isn't blank, count it as a number
+                    if (set.contains(num)) { // if the number is already in the set
+                        System.out.println("duplicate number in row " + r); // theres a duplicate
+                        return false;
+                    } else { // if its not
+                        set.add(num); // add it to the set
+                    }
+                }
+
+            }
+        }
+
+        // check columns
+        for (int c = 0; c < board[0].length; c++) { // start with a column
+            Set<Integer> set = new HashSet<Integer>();
+            for (int r = 0; r < board.length; r++) { // then check every row in that column
+
+                int num = board[r][c];
+
+                if (num != 0) { // if the num isn't blank, count it as a number
+                    if (set.contains(num)) { // if the number is already in the set
+                        System.out.println("duplicate number in column " + c); // theres a duplicate
+                        return false;
+                    } else { // if its not
+                        set.add(num); // add it to the set
+                    }
+                }
+
+            }
+        }
+
+        // check mini squares
+        for (int s = 1; s <= 9; s++) { // go through mini squares 1-9
+
+            Set<Integer> set = new HashSet<Integer>();
+            int[][] minisquare = miniSquare(s);
+
+            for (int c = 0; c < minisquare[0].length; c++) {
+                for (int r = 0; r < minisquare.length; r++) { // check each number in the mini square
+
+                    int num = minisquare[r][c];
+                    if (num != 0) { // if the num isn't blank, count it as a number
+                        if (set.contains(num)) { // if the number is already in the set
+                            System.out.println("duplicate number in mini square " + s); // theres a duplicate
+                            return false;
+                        } else { // if its not
+                            set.add(num); // add it to the set
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private int[][] miniSquare(int spot) {
+        int[][] mini = new int[3][3];
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                // whoa - wild! This took me a solid hour to figure out (at least)
+                // This translates between the "spot" in the 9x9 Sudoku board
+                // and a new mini square of 3x3
+                mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
+            }
+        }
+        return mini;
+    }
+
+
+
+    public boolean isSolved() {
+
+        if (!isValid()) { // check if the board is valid first
+            return false;
+        }
+
+        Map<Integer, Integer> numberCount = new HashMap<>(); // next count how many of each number is on the board
+        for (int[] row : board) {
+            for (int num : row) {
+                if (num != 0) { // if the num isn't blank, count it as a number
+                    if (numberCount.containsKey(num)) { // if the number is already in the map
+                        numberCount.put(num, numberCount.get(num) + 1);
+                    } else { // if its not
+                        numberCount.put(num, 1); // add it to the map
+                    }
+                }
+            }
+        }
+
+        for (Integer count : numberCount.values()) { // if the board doesn't have 9 of each number return false
+            if (count != 9) {
                 return false;
             }
-         }
-      }
+        }
+
       return true;
+
     }
-    
-    public boolean checkDuplicate() {
-      
-      //row
-      for(int r = 0; r < board.length; r++) {
-         Set<Integer> set = new HashSet<Integer>();
-         for(int c = 0; c < board[0].length; c++) {
-         
-            int num = board[r][c];
-            
-            if(num != 0){ // if the num isn't blank, count it as a number
-               if(set.contains(num)){ // if the number is already in the set
-                   System.out.println("duplicate number in row " + r); // theres a duplicate
-                   return false;
-               } else { // if its not
-                  set.add(num); // add it to the set
-               }
-            }
-            
-         }
-      }
-      
-      //columns
-      for(int c = 0; c < board[0].length; c++) {
-         Set<Integer> set = new HashSet<Integer>();
-         for(int r = 0; r < board.length; r++) {
-         
-            int num = board[r][c];
-            
-            if(num != 0){ // if the num isn't blank, count it as a number
-               if(set.contains(num)){ // if the number is already in the set
-                   System.out.println("duplicate number in column " + c); // theres a duplicate
-                   return false;
-               } else { // if its not
-                  set.add(num); // add it to the set
-               }
-            }
-            
-         }
-      }
-      
-      // blocks
-      for(int s = 1; s <= 9; s++){ // go through spots 1-9
-         Set<Integer> set = new HashSet<Integer>();
-         int[][] minisquare = miniSquare(s);
-         for(int c = 0; c < minisquare[0].length; c++) {   
-            for(int r = 0; r < minisquare.length; r++) {
-               int num = minisquare[r][c];
-               if(num != 0){ // if the num isn't blank, count it as a number
-                  if(set.contains(num)){ // if the number is already in the set
-                      System.out.println("duplicate number in mini square " + s); // theres a duplicate
-                      return false;
-                  } else { // if its not
-                     set.add(num); // add it to the set
-                  }
-               }
-            }
-         }
-      }
-      
-      return true;
-    }
-    
-    private int[][] miniSquare(int spot) {
-      int[][] mini = new int[3][3];
-      for(int r = 0; r < 3; r++) {
-         for(int c = 0; c < 3; c++) {
-            // whoa - wild! This took me a solid hour to figure out (at least)
-            // This translates between the "spot" in the 9x9 Sudoku board
-            // and a new mini square of 3x3
-            mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
-         }
-      }
-      return mini;
-   }
+
 
     public String toString() {
-      String result = "";
-      for(int r = 0; r < board.length; r++) {
-         for(int c = 0; c < board[0].length; c++) {
-            if(board[r][c] == 0) {
-               result += "X ";   
-            } else {
-               result += board[r][c] + " ";
+        String result = "";
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                if (board[r][c] == 0) {
+                    result += "X ";
+                } else {
+                    result += board[r][c] + " ";
+                }
             }
-         }
-         result += "\n";
-      }
+            result += "\n";
+        }
 
-      return result;
+        return result;
     }
- 
- 
- }
- 
- /*
+
+}
+
+/*
  2 X X 1 X 5 X X 3 
 X 5 4 X X X 7 1 X 
 X 1 X 2 X 3 X 8 X 
--- SudokuEngine.java
+++ SudokuEngine.java
@@ -1,7 +1,13 @@
 public class SudokuEngine {
     public static void main(String[] args) {
-        SudokuBoard myBoard = new SudokuBoard("data1.sdk");
-        System.out.println(myBoard.isValid());
+        SudokuBoard myBoard = new SudokuBoard("boards/valid-complete.sdk");
+        System.out.println("the board is valid: " + myBoard.isValid());
+        System.out.println("the board is solved: " + myBoard.isSolved());
         System.out.println(myBoard);
+
+        SudokuBoard myBoard2 = new SudokuBoard("boards/valid-incomplete.sdk");
+        System.out.println("the board is valid: " + myBoard2.isValid());
+        System.out.println("the board is solved: " + myBoard2.isSolved());
+        System.out.println(myBoard2);
     }
 }
